0a6666b5b90697ed643a85714d2e0003b6d5dd22
rubbor-duck <nelson.jordan10@hotmail.com>
2026-01-13 14:59:58 -0700

    W01 Prove: Developer—C# Programming Parts 1–2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is the percent of your grade? ");
+        string userInput = Console.ReadLine();
+        int percent = int.Parse(userInput);
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (90 > percent && percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (80 > percent && percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (70 > percent && percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+
+
+        {
+            Console.WriteLine($"You got an {letter}. ");
+        }
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Great Job! You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("Sadly, you didn't pass. You go this next time!");
+        }
     }
 }
