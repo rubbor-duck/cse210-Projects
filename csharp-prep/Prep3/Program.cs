fa6e0c4dc21b2e28177812f1bb73d60da0e90dd5
rubbor-duck <nelson.jordan10@hotmail.com>
2026-01-19 21:31:57 -0700

    Prep 3 Assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int random = randomGenerator.Next(1,100);
+
+        int guess = 0;
+
+        while (guess != random)
+        {
+            Console.Write("What is your guess? ");
+            string input = Console.ReadLine();
+            guess = int.Parse(input);
+            
+            if (guess > random)
+            {
+                Console.WriteLine("Lower");
+            }
+            
+            else if (guess == random)
+            {
+                continue;
+            }
+
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+
+        Console.WriteLine("You guessed it correctly!");
+
     }
+
 }
