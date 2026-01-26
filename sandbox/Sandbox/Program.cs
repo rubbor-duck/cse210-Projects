5d9e714b688c7d336afa7b5738b40b3972884182
rubbor-duck <nelson.jordan10@hotmail.com>
2026-01-25 20:32:27 -0700

    Prep 5 Maybe?!

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,30 @@
-using System;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 
-class Program
+for (int i = 0; i < 5; i++)
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Sandbox World!");
-    }
+    Console.WriteLine("Hello, World!");
+}
+
+//int total = 0;
+
+//for (int i = 0; i < 50; i++)
+//{
+//    total = total + 1;
+
+//    if (total % 2 !=0)
+//        Console.Write($"{total}:");
+//
+//    else
+//        continue;
+//    
+//}
+
+string deez_nutz = "Hello World";
+
+int size = deez_nutz.Length;
+
+for (int i = size - 1; i >=0; i--)
+{
+    Console.Write(deez_nutz[i]);
 }
