21f3ee6e64433887d0830f126cb6c895c5893786
rubbor-duck <nelson.jordan10@hotmail.com>
2026-01-27 16:37:19 -0700

    Learning 02 Done

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,26 @@
-using System;
+using System.ComponentModel.DataAnnotations;
 
-class Program
-{
-    static void Main(string[] args)
+public class Program{
+
+    public static void Main()
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Apple";
+        job1._jobTitle = "Fun Engineer";
+        job1._startYear = 1992;
+        job1._endYear = 2026;
+
+        Job job2 = new Job();
+        job2._company = "Meta";
+        job2._jobTitle = "Not Fun Engineer";
+        job2._startYear = 2000;
+        job2._endYear = 2030;
+
+        Resume myResume = new Resume();
+        myResume._name = "Bob Ross";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
