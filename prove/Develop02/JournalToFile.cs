6f759e4b50c4fe1d45f87d22e6f123c086f63df8
rubbor-duck <nelson.jordan10@hotmail.com>
2026-02-07 12:56:01 -0700

    Finished working on prove 2. Added a goals class for extra credit

--- prove/Develop02/JournalToFile.cs
+++ prove/Develop02/JournalToFile.cs
@@ -1,18 +1,176 @@
 using System.Formats.Asn1;
+
+//This class is the blueprint for each journal entry
+public class JournalEntry()
+{
+    public string _input;
+
+    // displays the lastest journal entry
+    public void Display()
+    {
+        Console.WriteLine();
+        Console.WriteLine(_input);
+        Console.WriteLine();
+    }
+
+    // gets input from user to write a jounral entry
+    // gets the date as well as gets the random prompt to display
+    public void GetInput()
+    {
+        string _date = GetDate();
+        
+        GetPrompt();
+
+        Console.Write($"{_date}: ");
+        _input = _date + Console.ReadLine();
+        
+        Console.WriteLine();
+    }
+
+    // chooses a random prompt by getting a random number, and pulls from an array of different prompts
+    public void GetPrompt()
+    {
+        List<string> _prompt = new List<string>();
+        _prompt.Add("What would nice thing would you say to your roommate?");
+        _prompt.Add("What nice thing has happend to you today?");
+        _prompt.Add("How did I see the hand of the Lord in my life today?");
+        _prompt.Add("What good did I do today?");
+        _prompt.Add("What would you like to improve upon?");
+        
+        Random _random = new Random();
+        int _randomNumber = _random.Next(0,5);
+        Console.WriteLine(_prompt[_randomNumber]);
+
+    }
+
+    // gets the current date from the comptuer
+    public string GetDate()
+    {
+        string _currentDate = DateTime.Now.ToString("M/d/yyyy: ");
+        return _currentDate;
+    }
+}
+
+// journal entry file manager
 public class JournalToFile
 {
 
-    public string FileName;
+    public string _fileName;
     
+    public List<JournalEntry> _currentJournal = new List<JournalEntry>();
+    
+    // adds the lastest journal entry to the file manager
+    public void AddEntry(JournalEntry _input)
+    {
+        _currentJournal.Add(_input);
+    }
 
+    // saves the file manager to a user specified file
     public void Save()
     {
-        
+        Console.Write("What file would you like to save to? ");
+        _fileName = Console.ReadLine();
+
+        using (StreamWriter _writer = new StreamWriter(_fileName))
+        {
+            //Saves the file as a bunch of strings with "|" separating them
+            string _journalString = string.Join("| ", _currentJournal.Select(item => item._input));
+            _writer.WriteLine(_journalString);
+        }
     }
 
+    // loads from a user specified file to the file manager
     public void Load()
     {
-        
+        _currentJournal.Clear();
+
+        Console.Write("What file would you like to read from? ");
+        _fileName = Console.ReadLine();
+
+        using (StreamReader _reader = new StreamReader(_fileName))
+        {
+            // reads from the file, and splits the txt into an array
+            string text = _reader.ReadToEnd();
+            string[] _journals = text.Split('|', StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string _p in _journals)
+            {
+            // iterates through the array and adds the journal entries to the _input attribute
+            JournalEntry _addJournal = new JournalEntry();
+            _addJournal._input = _p;
+
+            _currentJournal.Add(_addJournal);
+            }
+        }
     }
+
+    // displays all the journal entries from the file manager
+    public void DisplayAllJournals()
+    {
+        Console.WriteLine();
+
+        foreach (JournalEntry _x in _currentJournal)
+        {
+            Console.WriteLine(_x._input);
+        }
+
+        Console.WriteLine();
+    }
+    
+
+}
+
+// stores and shows goals at the beginning of the program
+public class Goals
+{
+    public string _workingGoal;
+
+    public string _goalFile = "goals.txt";
+
+    //Saves the goal into a file
+    public void SaveGoal()
+    {
+        File.WriteAllText(_goalFile, _workingGoal);
+    }
+
+    //Loads the goal from a file
+    public void LoadGoal()
+{
+    if (File.Exists(_goalFile))
+    {
+        _workingGoal = File.ReadAllText(_goalFile);
+    }
+    else
+    {
+        _workingGoal = "[No goal set yet]";
+    }
+}
+
+    // Sets the current working goal
+    public void CurrentGoal()
+    {
+        Console.Write("What goal do you want to work on throughout this week? ");
+        _workingGoal = Console.ReadLine();
+    }
+
+    // Displays the current working goal with a motivational quote added to it
+    public void DisplayGoal()
+    {
+        List<string> _goalMotivation = new List<string>();
+
+        _goalMotivation.Add("'The goal is not to be better than the other man, but your previous self.' —Dalai Lama");
+        _goalMotivation.Add("'Obstacles are those frightful things you see when you take your eyes off your goal.' ―Henry Ford");
+        _goalMotivation.Add("The Lord is proud of the effort you are making");
+        _goalMotivation.Add("'If a goal is worth having, it's worth blocking out the time in your day-to-day life necessary to achieve it.' —Jill Koenig");
+        _goalMotivation.Add("Our main goal is to become like Jesus. Remember, that He will send angels to aid you.");
+
+        Random _random = new Random();
+        int _randomNumber = _random.Next(0,5);
+
+        Console.WriteLine(_goalMotivation[_randomNumber]);
+        Console.WriteLine($"Goal: {_workingGoal}");
+    }
+
+
 
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,54 +1,126 @@
 using System;
 
+//I added a goals class to my program for the Show Creativity and Exceeding Requirements
+
+
+
 class Program
 {
-
+    // This function acts as the main select screen for the use to choose their option.
     static int SelectFunction()
     {
-        Console.Write($"Please Select a Function\n1. New Entry\n2. Display Journal\n3. Save Journal to File\n4. Load Journal from File\n5. Exit\nSelection: ");
+        Console.Write($"Please Select a Function\n0. Exit\n1. New Entry\n2. Display Latest Journal\n3. Display all Journals\n4. Save Journal to File\n5. Load Journal from File\n6. Set New Goal\nSelection: ");
 
         string preSelection;
         preSelection = Console.ReadLine();
         int selection = int.Parse(preSelection);
         return selection;
     }
-
+    
     static void Main(string[] args)
     {
+        int selection = -1;
+        
+        //creates a new journal entry and sets it to null
+        JournalEntry entry = null;
 
-    int selection = 0;
+        //creates a new journal manager that is used to save to a file and used to load from a file
+        JournalToFile jFile = new JournalToFile();
 
-    while (selection != 5)
+        //creates Goals class and loads the previous goal
+        Goals goal = new Goals();
+        goal.LoadGoal();
+
+        while (selection != 0)
         {
 
+            //calls the main menu to pop up
             selection = SelectFunction();
 
-
-        if (selection == 1)
+        // Exit loop. Asks if the user wants to save before closing the program
+        if (selection == 0)
             {
-                JournalEntry.NewEntry();
-            }
+                 for(;;)
+                {
+                    Console.Write("Do you want to save before you close the program? (y/n):  ");
+                    string save = Console.ReadLine();
+                
+               
 
-        else if (selection == 2)
-            {
-                JournalEntry.Display();
-            }
+                    if (save == "y")
+                    {
+                        jFile.Save();
+                        break; 
+                    }
+                    
+                    
+                    else if (save == "n")
+                        break;
 
-        else if (selection == 3)
-            {
-                JournalToFile.Save();
-            }
-
-        else if (selection == 4)
-            {
-                JournalToFile.Load();
-            }
-
-        else if (selection == 5)
-            {
-                continue;
+                    // if the user doesn't choose y or n, then it will pop up this error and loop back
+                    else
+                    {
+                        Console.WriteLine("please select either 'y' or 'n'");
+                        Console.WriteLine();
+                    }
+                }
             }
         
+        // calls for user input to create a new journal entry w/prompt. Saves the entry to the journal manager "jFile"
+        // Displays the current goal before asking for input
+        else if (selection == 1)
+            {
+                Console.WriteLine();
+                goal.DisplayGoal();
+                Console.WriteLine();
+                entry = new JournalEntry();
+                entry.GetInput();
+                jFile.AddEntry(entry);
+            }
+        
+        // Displays the last journal entry to be added to jFile. 
+        else if (selection == 2)
+            {
+                if (entry != null)
+                {
+                    entry.Display();
+                }
+
+                //If nothing has been added to jFile, it will display a message instead.
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("There is nothing to display");
+                    Console.WriteLine();
+                }
+            }
+
+        // Displays all the journal entries in jFile
+        else if (selection == 3)
+            {
+                jFile.DisplayAllJournals();
+            }
+
+        // Saves the journal entries to a user specified file
+        else if (selection == 4)
+            {
+                jFile.Save();
+            }
+
+        // Loads journal entries from a user specified file. Will overwrite what is already written in jFile.
+        else if (selection == 5)
+            {
+                jFile.Load();
+            }
+
+        // Allows the user to set the goal they want to work on
+        else if (selection == 6)
+            {
+                goal.CurrentGoal();
+                goal.SaveGoal();
+            }
+
+        // if the user chooses a number not in the menu, it will pop up this error message
         else
             {
                 Console.WriteLine("Please select a valid function");
